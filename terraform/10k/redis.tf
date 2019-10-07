bd819080322bbd06321ad4997625aa93d153dd4c
Grant Young <gyoung@gitlab.com>
2019-10-07 17:25:41 +0100

    Switch Redis to separate clusters

--- terraform/10k/redis.tf
+++ terraform/10k/redis.tf
@@ -1,14 +1,29 @@
-module "redis" {
+module "redis-cache" {
   source = "../modules/gitlab_gcp_instance"
 
   prefix = "${var.prefix}"
-  node_type = "redis"
-  node_count = 2
+  node_type = "redis-cache"
+  node_count = 3
 
   machine_type = "n1-highcpu-4"
   label_secondaries = true
 }
 
-output "redis" {
-  value = module.redis
+output "redis-cache" {
+  value = module.redis-cache
+}
+
+module "redis-persistent" {
+  source = "../modules/gitlab_gcp_instance"
+
+  prefix = "${var.prefix}"
+  node_type = "redis-persistent"
+  node_count = 3
+
+  machine_type = "n1-highcpu-4"
+  label_secondaries = true
+}
+
+output "redis-persistent" {
+  value = module.redis-persistent
 }
