ea487d4b7d560edadb786173f7e1a7edee0af387
Grant Young <gyoung@gitlab.com>
2019-10-11 17:47:29 +0100

    Add Jaeger for debugging

---
+++ terraform/10k/jaeger.tf
@@ -0,0 +1,34 @@
+module "jaeger" {
+  source = "../modules/gitlab_gcp_instance"
+
+  prefix = "${var.prefix}"
+  node_type = "jaeger"
+  node_count = 1
+
+  machine_type = "n1-standard-2"
+
+  tags = ["${var.prefix}-jaeger"]
+}
+
+resource "google_compute_firewall" "jaeger" {
+  ## firewall rules enabling the load balancer health checks
+  name    = "${var.prefix}-jaeger-firewall-rule"
+  network = "default"
+
+  description = "Allow Jaeger access"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["16686"]
+  }
+
+  target_tags   = ["${var.prefix}-jaeger"]
+}
+
+output "jaeger" {
+  value = module.jaeger
+}
