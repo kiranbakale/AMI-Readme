b9409060c12ea6510366901ccfff2e26537cfe50
Grant Young <gyoung@gitlab.com>
2019-11-12 14:20:51 +0000

    Remove old unneeded terraform firewall code

--- terraform/50k/firewall.tf
+++ terraform/50k/firewall.tf
@@ -14,7 +14,6 @@ resource "google_compute_firewall" "gitlab_http_https" {
     ports    = ["80", "443"]
   }
 
-  # source_ranges = ["35.191.0.0/16", "130.211.0.0/22", "209.85.152.0/22", "209.85.204.0/22"]
   target_tags   = ["${var.prefix}-web"]
 }
 
