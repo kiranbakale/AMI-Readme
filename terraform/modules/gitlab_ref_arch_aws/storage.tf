bf0af669d3caa1ead129f8422362433c4c3f8442
Paulo Martins <pmartins@gitlab.com>
2021-08-25 13:58:06 +0000

    fix s3 bucket

--- terraform/modules/gitlab_ref_arch_aws/storage.tf
+++ terraform/modules/gitlab_ref_arch_aws/storage.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "gitlab_object_storage_buckets" {
 
 resource "aws_iam_role" "gitlab_s3_role" {
   count = min(length(var.object_storage_buckets), 1)
-  name = "${var.prefix}-s3-role"
+  name  = "${var.prefix}-s3-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -23,10 +23,9 @@ resource "aws_iam_role" "gitlab_s3_role" {
   })
 }
 
-resource "aws_iam_role_policy" "gitlab_s3_policy" {
+resource "aws_iam_policy" "gitlab_s3_policy" {
   count = min(length(var.object_storage_buckets), 1)
-  name = "${var.prefix}-s3-policy"
-  role = aws_iam_role.gitlab_s3_role[0].id
+  name  = "${var.prefix}-s3-policy"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -44,8 +43,15 @@ resource "aws_iam_role_policy" "gitlab_s3_policy" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "s3_policy_profile_attachment" {
+  count = min(length(var.object_storage_buckets), 1)
+
+  policy_arn = aws_iam_policy.gitlab_s3_policy[0].arn
+  role       = aws_iam_role.gitlab_s3_role[0].name
+}
+
 resource "aws_iam_instance_profile" "gitlab_s3_profile" {
   count = min(length(var.object_storage_buckets), 1)
-  name = "${var.prefix}-s3-profile"
-  role = aws_iam_role.gitlab_s3_role[0].name
+  name  = "${var.prefix}-s3-profile"
+  role  = aws_iam_role.gitlab_s3_role[0].name
 }
