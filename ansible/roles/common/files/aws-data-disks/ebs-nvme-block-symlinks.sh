7079ab2edc4596e32f2c36a4925bf6e368d1bcc1
Grant Young <gyoung@gitlab.com>
2021-11-08 11:47:33 +0000

    Handle alternative block device path for AWS disk mounts

--- ansible/roles/common/files/aws-data-disks/ebs-nvme-block-symlinks.sh
+++ ansible/roles/common/files/aws-data-disks/ebs-nvme-block-symlinks.sh
@@ -4,7 +4,10 @@ for i in `seq 1 26`; do
     nvme_device_name="/dev/nvme${i}n1"
 
     if [ -e  $nvme_device_name ]; then
-        block_device_name=$(nvme id-ctrl ${nvme_device_name} --output binary | cut -c3073-3104 | tr -d '[:space:]')
+        block_device_name="$(nvme id-ctrl ${nvme_device_name} --output binary | cut -c3073-3104 | tr -d '[:space:]')"
+        if [[ $block_device_name != *"/dev"* ]]; then
+            block_device_name="/dev/${block_device_name}"
+        fi
 
         if [ -e $block_device_name ]; then
             echo "${block_device_name} > ${nvme_device_name} symlink already exists. Skipping..."
